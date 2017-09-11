5d5dc82935cd2aaac5dd84f3a55e396f4268a68d
LARumaguera <31608164+LARumaguera@users.noreply.github.com>
2017-09-11 22:50:29 +0800

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,23 +1,23 @@
 
 class Item {
-	String name;
-	double price;
+  String name;
+  double price;
 
-	Item(String name, double price){
-		this.name = name;
-		this.price = price;
-	}
-	// constants
-	final int SALE_LIMIT = 1000;
+  Item(String name, double price){
+    this.name = name;
+    this.price = price;
+  }
+  // constants
+  final int SALE_LIMIT = 1000;
 
-	// -> double
-	// Returns the sale price value given this sale percentage only if the price exceeds 1000.
+  // -> double
+  // Returns the sale price value given this sale percentage only if the price exceeds 1000.
 
-	double salePrice1000(double percentage){
-		if (price > SALE_LIMIT){
-			return price * (1 - percentage);
-		} else {
-			return price;
-		}
-	}
+  double salePrice1000(double percentage){
+    if (price > SALE_LIMIT){
+      return price * (1 - percentage);
+    } else {
+      return price;
+    }
+  }
 }
