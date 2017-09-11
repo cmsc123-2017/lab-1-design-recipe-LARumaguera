ae09458e54155f733bc2acbe6a7a4f86c7d7f62b
LARumaguera <31608164+LARumaguera@users.noreply.github.com>
2017-09-12 00:20:34 +0800

    Update JeepneyTripTest.java

--- JeepneyTripTest.java
+++ JeepneyTripTest.java
@@ -13,7 +13,7 @@ public class JeepneyTripTest extends TestCase {
    * many "testSomething" methods in this class as you wish, and each
    * one will be called when running JUnit over this class.)
    */
-  public void testTripCreated() {
+  public void testJeepneyTrip() {
     JeepneyTrip t = new JeepneyTrip(5, 5, 3);
     
     assertEquals(5.0, t.distance);
@@ -21,15 +21,67 @@ public class JeepneyTripTest extends TestCase {
     assertEquals(3, t.discountPassengers);
   }
   
-  public void testChangeNoDiscount() {
+  public void testFareChange() {
     JeepneyTrip t = new JeepneyTrip(5, 1, 0);
+    JeepneyTrip t1 = new JeepneyTrip(7, 2, 0);
+    JeepneyTrip t2 = new JeepneyTrip(5, 1, 1);
+    JeepneyTrip t3 = new JeepneyTrip(7, 1, 1);
     
-    assertEquals(3.0, t.fareChange(10));
+    assertEquals(13.0, t.fareChange(20));
+    assertEquals(4.0, t1.fareChange(20));
+    
+    assertEquals(4.0, t2.fareChange(10));
+    assertEquals(3.0, t3.fareChange(10));
   }
   
-  public void testChangeDiscount() {
-    JeepneyTrip t = new JeepneyTrip(5, 1, 1);
+  public void testTotalFarre(){
+    JeepneyTrip t = new JeepneyTrip(5, 1, 0);
+    JeepneyTrip t1 = new JeepneyTrip(7, 3, 1);
+    JeepneyTrip t2 = new JeepneyTrip(5, 1, 1);
+    JeepneyTrip t3 = new JeepneyTrip(7, 4, 0);
     
-    assertEquals(4.0, t.fareChange(10));
+    assertEquals(7.0, t.totalFare());
+    assertEquals(23.0, t1.totalFare());
+    assertEquals(6.0, t.totalFare());
+    assertEquals(29.0, t.totalFare());
   }
+  
+  public void testIsBeyond5Km(){
+    JeepneyTrip t = new JeepneyTrip(4, 3, 1);
+    JeepneyTrip t1 = new JeepneyTrip(7, 1, 0);
+    JeepneyTrip t2 = new JeepneyTrip(5, 4, 0);
+    
+    assertEquals(false, t.isBeyond5Km());
+    assertEquals(true, t1.isBeyond5Km());
+    assertEquals(false, t2.isBeyond5Km());
+  }
+  
+  public void testDiscountedFare(){
+    JeepneyTrip t = new JeepneyTrip(5, 1, 0);
+    JeepneyTrip t1 = new JeepneyTrip(7, 2, 0);
+    JeepneyTrip t2 = new JeepneyTrip(5, 1, 1);
+    JeepneyTrip t3 = new JeepneyTrip(7, 1, 1);
+    JeepneyTrip t4 = new JeepneyTrip(7, 2, 1);
+    
+    assertEquals(0.0, t.discountedFare());
+    assertEquals(0.0, t1.discountedFare());
+    assertEquals(6.0, t2.discountedFare());
+    assertEquals(7.0, t3.discountedFare());
+    assertEquals(7.0, t4.discountedFare());
+  }
+  
+   public void testNoDiscountFare(){
+    JeepneyTrip t = new JeepneyTrip(5, 1, 0);
+    JeepneyTrip t1 = new JeepneyTrip(7, 2, 0);
+    JeepneyTrip t2 = new JeepneyTrip(5, 1, 1);
+    JeepneyTrip t3 = new JeepneyTrip(7, 1, 1);
+    JeepneyTrip t4 = new JeepneyTrip(7, 2, 1);
+    
+    assertEquals(7.0, t.noDiscountFare());
+    assertEquals(15.0, t1.noDiscountFare());
+    assertEquals(0.0, t2.noDiscountFare());
+    assertEquals(0.0, t3.noDiscountFare());
+    assertEquals(8.0, t4.noDiscountFare());
+  }
+  
 }
